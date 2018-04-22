816eaaac6aa78d9b52f3d2b7e223f1dec6f659bd
rkalz <aleezada@gmail.com>
2018-04-22 16:54:37 -0500

    Fixed weird memory bugs with decrypting

--- RC4_HMAC_MD5/RC4HMACMD5.h
+++ RC4_HMAC_MD5/RC4HMACMD5.h
@@ -5,7 +5,6 @@
 #include "string.h"
 
 #include <openssl\hmac.h>
-#include <openssl\md4.h>
 #include <openssl\md5.h>
 #include <openssl\rand.h>
 #include <openssl\rc4.h>
@@ -16,24 +15,24 @@
 
 void decrypt(const unsigned char* K, const uint32_t T, unsigned char* edata, const int edata_len) {
 
-	int data_len = edata_len - HMAC_MD5_LENGTH + CONFOUNDER_LENGTH;
-	// K1 == K2 when not using EXP
+	int data_len = edata_len - HMAC_MD5_LENGTH - CONFOUNDER_LENGTH;
+	
+	// Initialize K1 (same as K2 without EXP) and K3)
 	unsigned char K1[HMAC_MD5_LENGTH];
-	HMAC(EVP_md5(), K, KEY_LENGTH, &T, 4, K1, NULL); // HMAC(K, &T, 4, K1)
+	HMAC(EVP_md5(), K, KEY_LENGTH, &T, 4, K1, NULL); 
+	unsigned char K3[HMAC_MD5_LENGTH];
+	HMAC(EVP_md5(), K1, HMAC_MD5_LENGTH, edata, HMAC_MD5_LENGTH, K3, NULL); 
 
 	// Decrypt edata
-	unsigned char K3[HMAC_MD5_LENGTH];
-	HMAC(EVP_md5(), K1, HMAC_MD5_LENGTH, edata, HMAC_MD5_LENGTH, K3, NULL); // K3 = HMAC(K1, edata.Checksum)
 	RC4_KEY rc4_key;
 	RC4_set_key(&rc4_key, HMAC_MD5_LENGTH, K3);
-	RC4(&rc4_key, CONFOUNDER_LENGTH, edata + HMAC_MD5_LENGTH, edata + HMAC_MD5_LENGTH);  // RC4(K3, edata.Confounder)
-	RC4(&rc4_key, data_len, edata + HMAC_MD5_LENGTH + CONFOUNDER_LENGTH, edata + HMAC_MD5_LENGTH + CONFOUNDER_LENGTH); // RC4(edata.Data)
+	RC4(&rc4_key, CONFOUNDER_LENGTH, edata + HMAC_MD5_LENGTH, edata + HMAC_MD5_LENGTH);  
+	RC4(&rc4_key, data_len, edata + HMAC_MD5_LENGTH + CONFOUNDER_LENGTH, edata + HMAC_MD5_LENGTH + CONFOUNDER_LENGTH); 
 
-	// verify with checksum
-	unsigned char* checksum = HMAC(EVP_md5(), K1, HMAC_MD5_LENGTH, edata + HMAC_MD5_LENGTH, edata_len - HMAC_MD5_LENGTH, NULL, NULL);
+	// Verify with checksum
+	unsigned char checksum[HMAC_MD5_LENGTH];
+	HMAC(EVP_md5(), K1, HMAC_MD5_LENGTH, edata + HMAC_MD5_LENGTH, edata_len - HMAC_MD5_LENGTH, checksum, NULL);
 	if (memcmp(checksum, edata, HMAC_MD5_LENGTH)) printf("Checksum failed!\n");
-
 }
-
 
 #endif
--- RC4_HMAC_MD5/main.c
+++ RC4_HMAC_MD5/main.c
@@ -2,14 +2,23 @@
 
 int main(void) {
 	const unsigned char* key = "\x22\x66\x57\xA0\x62\xEE\xA9\xD7\x13\x76\x02\x16\x81\xAE\x2A\xBA";
-	const unsigned char encdata[] = "\x5c\x13\x4c\x89\x52\x60\x44\xdd\xba\x3a\x0a\x37\x64\xd9\xc3\x12" \
+	unsigned char* encdata = (unsigned char*)malloc(52);
+	memcpy_s(encdata, 52, "\x5c\x13\x4c\x89\x52\x60\x44\xdd\xba\x3a\x0a\x37\x64\xd9\xc3\x12" \
 		"\x7d\xe7\xcb\x3a\x94\x09\x6a\x5b\x72\x19\x5e\x6c\xae\x4e\x96\x04" \
 		"\x2f\xfc\xbf\x26\x64\xb1\xf6\xd6\x1b\x3b\x2d\x73\x57\xfe\x32\x24" \
-		"\x8f\x87\x43\xdf";
+		"\x8f\x87\x43\xdf", 52);
 
-	decrypt(key, 0x00000001, &encdata, 52);
-	printf("%s\n", encdata);
+	decrypt(key, 0x00000001, encdata, 52);
+	
+	printf("Checksum: ");
+	for (int i = 0; i < HMAC_MD5_LENGTH; ++i) printf("%02X ", encdata[i]);
+	printf("\nConfounder: ");
+	for (int i = HMAC_MD5_LENGTH; i < HMAC_MD5_LENGTH + CONFOUNDER_LENGTH; ++i) printf("%02X ", encdata[i]);
+	printf("\nData: ");
+	for (int i = HMAC_MD5_LENGTH + CONFOUNDER_LENGTH; i < 52; ++i) printf("%c", encdata[i]);
+	printf("\n");
 
 	system("pause");
+	free(encdata);
 	return 0;
 }
