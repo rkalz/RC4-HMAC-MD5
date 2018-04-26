6574de88c6edc7caace5b27ebfadf364ff26a861
rkalz <aleezada@gmail.com>
2018-04-26 14:38:31 -0500

    Clarified example varaible names

--- RC4_HMAC_MD5/main.c
+++ RC4_HMAC_MD5/main.c
@@ -1,20 +1,21 @@
 #include "RC4_HMAC_MD5.h"
 
 int main(void) {
-	/*const int len = 37;
+	/*const char* data = "generalkenobi";
+	const int enclen = HMAC_MD5_LENGTH + CONFOUNDER_LENGTH + strlen(data);
 	const unsigned char* key = "hellothere";
-	const unsigned char* encdata = encrypt(key, 1, "generalkenobi", strlen("generalkenobi"));
-	int result = decrypt(key, 1, encdata, len);*/
+	const unsigned char* encdata = encrypt(key, 1, data, strlen(data));
+	int result = decrypt(key, 1, encdata, enclen);*/
 
-	const int len = 52;
+	const int enclen = 52;
 	const unsigned char* key = "\x22\x66\x57\xA0\x62\xEE\xA9\xD7\x13\x76\x02\x16\x81\xAE\x2A\xBA";
-	unsigned char* encdata = (unsigned char*)malloc(len);
-	memcpy_s(encdata, len, "\x5c\x13\x4c\x89\x52\x60\x44\xdd\xba\x3a\x0a\x37\x64\xd9\xc3\x12" \
+	unsigned char* encdata = (unsigned char*)malloc(enclen);
+	memcpy_s(encdata, enclen, "\x5c\x13\x4c\x89\x52\x60\x44\xdd\xba\x3a\x0a\x37\x64\xd9\xc3\x12" \
 		"\x7d\xe7\xcb\x3a\x94\x09\x6a\x5b\x72\x19\x5e\x6c\xae\x4e\x96\x04" \
 		"\x2f\xfc\xbf\x26\x64\xb1\xf6\xd6\x1b\x3b\x2d\x73\x57\xfe\x32\x24" \
-		"\x8f\x87\x43\xdf", len);
+		"\x8f\x87\x43\xdf", enclen);
 
-	int result = decrypt(key, 1, encdata, 52);
+	int result = decrypt(key, 1, encdata, enclen);
 
 	if (result) {
 		printf("Checksum: ");
@@ -22,7 +23,7 @@ int main(void) {
 		printf("\nConfounder: ");
 		for (int i = HMAC_MD5_LENGTH; i < HMAC_MD5_LENGTH + CONFOUNDER_LENGTH; ++i) printf("%02X ", encdata[i]);
 		printf("\nData: ");
-		for (int i = HMAC_MD5_LENGTH + CONFOUNDER_LENGTH; i < len; ++i) printf("%c", encdata[i]);
+		for (int i = HMAC_MD5_LENGTH + CONFOUNDER_LENGTH; i < enclen; ++i) printf("%c", encdata[i]);
 		printf("\n");
 	} else printf("Checksum failed!\n");
 
